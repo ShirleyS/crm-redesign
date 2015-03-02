1439ae758e7d1c902ea3b54aad368c1b8043a628
Shirley Shaw <shirleyshaw.is@gmail.com>
2015-03-02 12:14:57 -0800

    cleaned up interactions

--- app/js/custom.js
+++ app/js/custom.js
@@ -19,14 +19,42 @@ $(function() {
     $expand = $(this).find(">:first-child");
  
     if($expand.text() == "+") {
-      self.parent().css("background-color", "#c6d0da");
+      // $(this).addClass('active');
+      self.parent().css("background-color", "white");
+      self.parent().css("height", "100%");
+      
       $expand.text("-");
+      $expand.text("-").css("font-weight","bold");
+
      
     } 
     else {
       self.parent().css("background-color", "white");
+      self.parent().css("height", "24px");
       $expand.text("+");
+      $expand.text("+").css("font-weight","bold");
     }
 
   });
 });
+
+
+function sendSpec () {
+  $("#sendSpec").slideToggle("slow");
+  if($('#sendWhen').html() == 'IMMEDIATLY'){
+     $('#sendWhen').html('AS SPECIFIED');
+     $('#sendWhenState').val('1');
+  }
+  else{
+     $('#sendWhen').html('IMMEDIATLY');
+     $('#sendWhenState').val('0');
+  }}
+
+$("#sendChange").click(function () {
+  sendSpec();
+  return false;
+});
+if($('#sendWhenState').val() == 1){
+  sendSpec();
+}
+
